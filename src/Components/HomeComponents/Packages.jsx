765c8ade60c3a96f4d339644d0872be511665de2
unknown <shaimazerin46@gmail.com>
2025-03-27 11:46:52 +0600

    package section updated

--- src/Components/HomeComponents/Packages.jsx
+++ src/Components/HomeComponents/Packages.jsx
@@ -3,7 +3,6 @@ import useAxiosPublic from "../../hooks/useAxiosPublic";
 import Headline from "../Headline/Headline";
 import { Link } from "react-router";
 import WebButton from "../WebButton/WebButton";
-import { GrWaypoint } from "react-icons/gr";
 
 
 const Packages = () => {
@@ -22,31 +21,24 @@ const Packages = () => {
             <Headline text={"Packages"}></Headline>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {packages?.map(p => (
-                    <div
-                        key={p._id}
-                        className="relative bg-gradient-to-br from-[#98D2C0] to-[#F6F8D5] p-6 rounded-3xl shadow-xl hover:scale-105 transition-transform duration-300 ease-in-out"
-                    >
-                        <h3 className="text-2xl font-bold mb-4 text-center">
-                            {p?.name}
-                        </h3>
-                        <ul className="list-disc text-sm space-y-2 mb-6 pl-5">
-                            {p?.features?.length > 0 && (
-                                p.features.map((feature, index) => (
-                                   <div className="flex gap-3">
-                                     <GrWaypoint />
-                                    <li key={index} className="flex items-center gap-2">
-                                        {feature}
-                                    </li>
-                                   </div>
-                                ))
-                            )}
-                        </ul>
-                        <div className="flex justify-between items-center">
-                            <span className="text-xl font-bold ">${p?.price}</span>
-                            <Link>
-                                <WebButton text={"Buy now"}></WebButton>
-                            </Link>
+                    <div key={p._id} className="w-96 shadow-xl h-full flex flex-col bg-gradient-to-b from-[#98D2C0] rounded-2xl to-[#F6F8D5]">
+                        <h3 className="py-3 bg-[#3b997b] rounded-t-2xl text-white text-center">{p?.name}</h3>
+                        <div className="p-5 flex flex-col grow ">
+                           <div className="flex items-center justify-between">
+                           <div className="flex items-center h-25 w-25 rounded-full bg-gradient-to-r text-white from-yellow-500 to-yellow-300 shadow-lg justify-center">
+                                <span>BDT {p?.price}</span>
+                            </div><span className="ml-3 py-2 px-3 bg-green-200 text-sm border-[1px] border-green-300 rounded-3xl text-black ">Month</span>
+                           </div>
+                            <ul className="mt-5">
+                                {
+                                    p?.features.map((feature, idx) => <li className="mb-3 flex items-center gap-2" key={idx}><span className="w-2 h-2 bg-[#3b997b] rounded-full"></span>{feature}</li>)
+                                }
+                            </ul>
+
                         </div>
+                        <Link className="text-center pb-5">
+                            <WebButton text={"Subscribe"}></WebButton>
+                        </Link>
                     </div>
                 ))}
             </div>
