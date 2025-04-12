eac901afc28e21c67d526a1e4e91451b8d31c979
unknown <shaimazerin46@gmail.com>
2025-04-12 19:44:17 +0600

    modified package card design

--- src/Components/HomeComponents/Packages.jsx
+++ src/Components/HomeComponents/Packages.jsx
@@ -4,6 +4,7 @@ import Headline from "../Headline/Headline";
 import { Link } from "react-router";
 import WebButton from "../WebButton/WebButton";
 import { Parallax } from "react-scroll-parallax";
+import bg from '../../assets/subscription/bg.jpg'
 
 
 
@@ -22,19 +23,27 @@ const Packages = () => {
       <Parallax  translateY={[-20, 20]} speed={-5} className="mt-30">
         
 
-<div className=" bg-[#98d2c0]">
+<div className="">
             <Headline text={"Packages"}></Headline>
             <div className="grid max-w-7xl mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pb-20">
                 {packages?.map(p => (
-                    <div key={p._id} className=" transition-transform duration-300 hover:scale-105 shadow-xl h-full flex flex-col  rounded-2xl bg-white">
-                        <div className="py-5 h-full rounded-t-2xl" style={{background: 'url(./card.svg)', backgroundRepeat: 'no-repeat', }}>
-                        <h3 className="py-3 font-bold text-center">{p?.name}</h3>
+                    <div key={p._id}
+                    style={{background: `url(${bg})`}}
+                     className="transition-transform duration-300 hover:scale-105 shadow-xl h-full flex flex-col  rounded-2xl ">
+                        <div className="py-5 h-full rounded-t-2xl" >
+                        <h3 className="py-3 text-xl text-[#4F959D] font-bold text-center">{p?.name}</h3>
                         <h3 className="text-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-600 to-blue-600">BDT {p?.price}/Month</h3>
 
                         <div className="p-5 flex flex-col grow">
                             <ul className="mt-5">
                                 {
-                                    p?.features.map((feature, idx) => <li className="mb-3 flex items-center gap-2" key={idx}><span className="w-2 h-2 bg-[#3b997b] rounded-full"></span>{feature}</li>)
+                                    p?.features.map((feature, idx) => (
+                                        <li className="mb-3 text-white border-b-[1px] p-2 text-center text-sm" key={idx}>{feature}
+                                        </li>
+                                    
+                                    )
+
+                                )
                                 }
                             </ul>
 
@@ -48,13 +57,7 @@ const Packages = () => {
                     </div>
                 ))}
             </div>
-            <div class="wave">
-            
-            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" fill="white" fillRule="evenodd"></path>
-    </svg>
-
-</div>
+           
         </div>
       </Parallax>
     );
