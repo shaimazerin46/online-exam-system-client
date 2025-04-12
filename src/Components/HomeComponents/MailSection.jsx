d602cfcc0db4b7459014756255ad12f71d5812f4
unknown <shaimazerin46@gmail.com>
2025-04-12 20:51:02 +0600

    emailjs implemented doen

--- src/Components/HomeComponents/MailSection.jsx
+++ src/Components/HomeComponents/MailSection.jsx
@@ -3,23 +3,46 @@ import bg from '../../assets/mail/bg.png'
 import mailLottie from '../../assets/lottie/mail.json'
 import Lottie from "lottie-react";
 import WebButton from "../WebButton/WebButton";
+import { useContext, useState } from "react";
+import emailjs from '@emailjs/browser';
+import { AuthContext } from "../../Context/AuthProvider";
 
 
 const MailSection = () => {
+    const [message,setMessage] = useState('');
+    const {user} = useContext(AuthContext);
+
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const data = {
+            
+            email: user?.email,
+            text: message
+        }
+        emailjs.send(
+            'service_942dfyw',
+            'template_m1r9txs',
+            data,
+            'Q0wGHAYPZtgl_MkzK'
+        )
+        .then(res=>console.log(res.text))
+        .catch(err=>console.log(err.text))
+    }
     return (
         <div className="max-w-7xl mx-auto">
             <Headline text={"Contact us"}></Headline>
             <div className="flex items-center justify-center shadow-2xl rounded-3xl">
-                <div style={{background: `url(${bg})`, backgroundRepeat: "no-repeat"}} className="p-10 w-[500px] rounded-xl">
+                <form  style={{background: `url(${bg})`, backgroundRepeat: "no-repeat"}} className="p-10 w-[500px] rounded-xl">
                 
-                    <input type="text" placeholder="Type your name" className="input w-full rounded-l-3xl rounded-r-3xl focus:outline-0 focus:border-0 border-0 shadow-2xl"  /> <br/><br/>
                    
-                    <textarea className="textarea resize-none w-full rounded-l-3xl rounded-r-3xl focus:outline-0 focus:border-0 border-0 shadow-2xl" placeholder="Enter your message"></textarea><br/><br/>
+                   
+                    <textarea required name="text" value={message} onChange={(e) => setMessage(e.target.value)} className="textarea resize-none w-full rounded-l-3xl rounded-r-3xl focus:outline-0 focus:border-0 border-0 shadow-2xl" placeholder="Enter your message"></textarea><br/><br/>
 
-                    <div className="flex justify-center">
+                    <div onClick={handleSubmit} className="flex justify-center">
                     <WebButton text={"Send"}></WebButton>
                     </div>
-                </div>
+                </form>
                 <div className="w-[500px]">
                     <Lottie animationData={mailLottie}></Lottie>
                 </div>
